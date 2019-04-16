f49bea82d3c6328110b0a2e667bda07df01597fd
MikeKowalsky <mkowalewski83@gmail.com>
2019-04-16 10:59:21 +0200

    basic server config

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const path = require("path");
+
+const express = require("express");
+const pug = require("pug");
+
+const app = express();
+
+app.set("view engine", "pug");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server on ${process.env.PORT}`);
+});
